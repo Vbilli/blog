c033e4bbd53bb468521fe7b102cbc97d49d624c0
liuhuibing <liuhuibing163@sina.com>
2015-12-12 13:43:36 +0800

    still me

---
+++ Blog/Content/JS/Pop.js
@@ -0,0 +1,22 @@
+﻿
+
+$(document).ready(function () {
+    document.addEventListener("click", function (event) {
+        if (event.target.className == "Pop") {
+            $(".Popupform").attr("display", "block");
+        }
+
+    });
+    //swal({
+    //    title: 'Ajax request example',
+    //    text: 'Submit to run ajax request',
+    //    type: 'info',
+    //    showCancelButton: true,
+    //    closeOnConfirm: false,
+    //    showLoaderOnConfirm: true,
+    //}, function () {
+    //    setTimeout(function () {
+    //        swal('Ajax request finished!');
+    //    }, 2000);
+    //});
+})
---
+++ Blog/Controllers/AccountController.cs
@@ -0,0 +1,141 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+using System.Web.WebPages;
+using System.Web.Security;
+using Blog.Filters;
+
+namespace Blog.Controllers
+{
+    [Authorize]
+    [InitializeSimpleMembership]
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel login, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(login.UserName, login.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(login.UserName, login.RememberMe);
+                    if (Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return RedirectToAction("Index", "Home", login);
+
+            //if (ModelState.IsValid && WebSecurity.Login(login.UserName, login.Password, persistCookie: true))
+            //{
+            //    return RedirectToLocal(returnUrl);
+            //}
+            //else
+            //{
+            //    return RedirectToAction("Index", "Home");
+            //}
+
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    WebSecurity.CreateUserAndAccount(model.UserName, model.Password, false);
+                    WebSecurity.Login(model.UserName, model.Password);
+                    return RedirectToAction("Index", "Home");
+                }
+                catch (MembershipCreateUserException e)
+                {
+                    ModelState.AddModelError("", ErrorCodeToString(e.StatusCode));
+                }
+            }
+            return View(model);
+        }
+        private static string ErrorCodeToString(MembershipCreateStatus createStatus)
+        {
+            // See http://go.microsoft.com/fwlink/?LinkID=177550 for
+            // a full list of status codes.
+            switch (createStatus)
+            {
+                case MembershipCreateStatus.DuplicateUserName:
+                    return "User name already exists. Please enter a different user name.";
+
+                case MembershipCreateStatus.DuplicateEmail:
+                    return "A user name for that e-mail address already exists. Please enter a different e-mail address.";
+
+                case MembershipCreateStatus.InvalidPassword:
+                    return "The password provided is invalid. Please enter a valid password value.";
+
+                case MembershipCreateStatus.InvalidEmail:
+                    return "The e-mail address provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidAnswer:
+                    return "The password retrieval answer provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidQuestion:
+                    return "The password retrieval question provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidUserName:
+                    return "The user name provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.ProviderError:
+                    return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                case MembershipCreateStatus.UserRejected:
+                    return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                default:
+                    return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+            }
+        }
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+    }
+}
